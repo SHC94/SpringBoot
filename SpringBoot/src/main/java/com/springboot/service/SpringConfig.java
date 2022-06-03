5d8c84671f7536d08d88e743cb0fb16b5a46274a
SHC <gudcjf4757@naver.com>
2022-06-03 19:58:05 +0900

    H2 DB, JDBC

--- SpringBoot/src/main/java/com/springboot/service/SpringConfig.java
+++ SpringBoot/src/main/java/com/springboot/service/SpringConfig.java
@@ -1,22 +1,31 @@
 package com.springboot.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.springboot.repository.JdbcMemberRepository;
 import com.springboot.repository.MemberRepository;
-import com.springboot.repository.MemoryMemberRepository;
 
 //자바 코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
+	private final DataSource dataSource;
+
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 //	@Bean
 //	public MemberService memberService() {
 //		return new MemberService(memberRepository());
 //	}
-//	
-//	@Bean
-//	public MemberRepository memberRepository() {
-//		return new MemoryMemberRepository();
-//	}
+
+	@Bean
+	public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
+	}
 }
